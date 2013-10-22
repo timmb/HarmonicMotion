693680f21e1551a6bbcde20d482245814dc47c9e
Tim Murray-Browne <@>
2013-10-22 17:39:05 +0100

    Added extra libs to cmake to make it compile against hm lib

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 	QApplication app(argc, argv);
 	QMainWindow* w = new QMainWindow;
 	w->show();
-	Pipeline pipeline;
-	qDebug() << pipeline.test();
+	hm::Pipeline pipeline;
+//	qDebug() << pipeline.test();
 	return app.exec();
 }
