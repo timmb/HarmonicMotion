5f8b034313f68fb45dd39b7fbb58bf6b0c8a1a5b
Tim Murray-Browne <tim@timmb>
2014-06-02 19:56:26 +0100

    change initial patch for expression node

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -26,57 +26,20 @@ Controller::Controller(QObject* parent)
 , mRedrawIsRequired(false)
 {
 	mPipeline = mMainWindow->patchArea()->pipeline();
-	
-//	NodePtr kinect = NodePtr(new NodeKinect);
-//	mNodes << kinect;
-//	mPipeline.addNode(kinect);
 	NodePtr gen = FactoryNode::instance()->create("NodeSineWave");
-//	mNodes << gen;
 	mPipeline->addNode(gen);
-//	mMainWindow->patchArea()->addNode(gen);
 	
-//	mAccum = std::shared_ptr<NodeAccumulator>(new NodeAccumulator);
-////	kinect->outlet(0)->connect(mAccum->inlet(0));
-//	mNodes << mAccum;
-//	mPipeline.addNode(mAccum);
-
+	NodePtr expr = FactoryNode::instance()->create("NodeExpression");
+	mPipeline->addNode(expr);
+	
+	NodePtr print = FactoryNode::instance()->create("NodePrinter");
+	mPipeline->addNode(print);
+	
+	mPipeline->connect(gen->outlet(0), expr->inlet(0));
+	mPipeline->connect(expr->outlet(0), print->inlet(0));
+	
 	bool success = connect(this, SIGNAL(newConsoleMessage(QString)), mMainWindow, SLOT(newConsoleMessage(QString)));
 	assert(success);
-	
-	NodePtr osc = FactoryNode::instance()->create("NodeOscOut");
-//	kinect->outlet(0)->connect(osc->inlet(0));
-	mPipeline->addNode(osc);
-//	mPipeline->connect(gen->outlet(0), osc->inlet(0));
-//	mMainWindow->patchArea()->addNode(osc);
-	
-////	auto filter = NodePtr(new NodeFilter);
-////	kinect->outlet(0)->connect(filter->inlet(0));
-////	mPipeline.addNode(filter);
-//	
-//	auto filterOut = NodePtr(new OutputConsolePrinter);
-//	filter->outlet(0)->connect(filterOut->inlet(0));
-//	kinect->outlet(0)->connect(filterOut->inlet(0));
-//	mPipeline.addNode(filterOut);
-//	
-//	auto renderer0 = NodeRenderer::Ptr(new NodeRenderer);
-//	mPipeline.addNode(renderer0);
-//	
-//	auto renderer1 = NodeRenderer::Ptr(new NodeRenderer);
-//	mPipeline.addNode(renderer1);
-//	
-//	filter->outlet(0)->connect(renderer0->inlet(0));
-//	kinect->outlet(0)->connect(renderer1->inlet(0));
-//	
-//	NodeRendererGlWidget* w = mMainWindow->createRendererWidget();
-//	w->setRenderer(renderer0);
-//	auto w2 = mMainWindow->createRendererWidget();
-//	w2->setRenderer(renderer1);
-	
-	
-	auto timer = new QTimer(this);
-	connect(timer, SIGNAL(timeout()), this, SLOT(checkPipeline()));
-	timer->setInterval(10);
-	timer->start();
 	
 	mMainWindow->show();
 	mPipeline->start();
@@ -84,11 +47,5 @@ Controller::Controller(QObject* parent)
 
 void Controller::checkPipeline()
 {
-	if (mAccum)
-	while (!mAccum->isEmpty())
-	{
-		Data data = mAccum->next();
-		Q_EMIT newConsoleMessage(QString::fromStdString(data.toString()));
-//		std::cout << "emit new console message "<<data<<std::endl;
-	}
+
 }
