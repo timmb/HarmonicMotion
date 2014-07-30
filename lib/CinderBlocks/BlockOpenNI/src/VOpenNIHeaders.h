1e798a3356515daa661ddc78d3ca7d3ff1468522
Tim Murray-Browne <tim@timmb>
2014-07-30 19:58:50 +0100

    Added VOpenNI block to repository rather than having it as a submodule

---
+++ lib/CinderBlocks/BlockOpenNI/src/VOpenNIHeaders.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "VOpenNICommon.h"
+// #ifdef _WIN32
+// #include "VOpenNINetwork.h"
+// #endif
+#include "VOpenNIBone.h"
+#include "VOpenNIDevice.h"
+#include "VOpenNIDeviceManager.h"
+#include "VOpenNISurface.h"
+#include "VOpenNIUser.h"
+
